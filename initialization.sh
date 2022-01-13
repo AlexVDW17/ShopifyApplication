764ef5ed0e98f5c0e193d7c05da4dd3a4d4dd6bd
Alex Van De Wiele <avandewi@uwaterloo.ca>
2022-01-13 14:09:26 -0500

    trying to make heroku work

--- initialization.sh
+++ initialization.sh
@@ -1,2 +1,3 @@
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
+
